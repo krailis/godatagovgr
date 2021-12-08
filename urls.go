0e4d8ad85425f888acf96dc4d1aa968ad466c218
Konstantinos Railis <krailis@hotmail.com>
2021-12-08 23:00:51 +0200

    Added complete collection of dataset URLs

--- urls.go
+++ urls.go
@@ -3,45 +3,57 @@ package godatagovgr
 const (
 	// Business.
 	// See more: https://www.data.gov.gr/search/?topic=business
-	URL_ACCOUNTANT_NUMBERS       = "oee_accountants"
-	URL_AUDITOR_NUMBERS          = "elte_auditors"
-	URL_CASINO_TICKET_NUMBERS    = "eeep_casino_tickets"
-	URL_ENERGY_INSPECTOR_NUMBERS = "minenv_inspectors"
-	URL_REALTOR_NUMBERS          = "mindev_realtors"
-	URL_TOURIST_AGENCY_NUMBERS   = "mintour_agencies"
+	URL_ACCOUNTANTS       = "oee_accountants"
+	URL_AUDITORS          = "elte_auditors"
+	URL_CASINO_TICKETS    = "eeep_casino_tickets"
+	URL_ENERGY_INSPECTORS = "minenv_inspectors"
+	URL_REALTORS          = "mindev_realtors"
+	URL_TOURIST_AGENCIES  = "mintour_agencies"
 
 	// Crime.
 	// See more: https://www.data.gov.gr/search/?topic=crime
-	URL_TRAFFIC_ACCIDENT_DATA  = "mcp_traffic_accidents"
-	URL_TRAFFIC_VIOLATION_DATA = "mcp_traffic_violations"
-	URL_HCG_INCIDENT_DATA      = "hcg_incidents"
-	URL_CRIME_STATS_DATA       = "mcp_crime"
-	URL_FINANCIAL_CRIME_DATA   = "mcp_financial_crimes"
-	URL_LAW_FIRM_NUMBERS       = "minjust_law_firms"
-	URL_LAWYER_NUMBERS         = "minjust_lawyers"
+	URL_TRAFFIC_ACCIDENTS  = "mcp_traffic_accidents"
+	URL_TRAFFIC_VIOLATIONS = "mcp_traffic_violations"
+	URL_HCG_INCIDENTS      = "hcg_incidents"
+	URL_CRIMES             = "mcp_crime"
+	URL_FINANCIAL_CRIMES   = "mcp_financial_crimes"
+	URL_LAW_FIRMS          = "minjust_law_firms"
+	URL_LAWYERS            = "minjust_lawyers"
 
 	// Education.
 	// See more: https://www.data.gov.gr/search/?topic=education
-	URL_ACADEMIC_PROFESSOR_DATA  = "minedu_dep"
-	URL_STUDENT_SCHOOL_DATA      = "minedu_students_school"
-	URL_INTERNSHIP_DATA          = "grnet_atlas"
-	URL_EUDOXUS_APPLICATION_DATA = "grnet_eudoxus"
+	URL_ACADEMIC_PROFESSORS  = "minedu_dep"
+	URL_STUDENT_SCHOOLS      = "minedu_students_school"
+	URL_INTERNSHIPS          = "grnet_atlas"
+	URL_EUDOXUS_APPLICATIONS = "grnet_eudoxus"
 
 	// Environment.
 	// See more: https://data.gov.gr/search/?topic=environment
-	URL_OWNERS_PER_MUNICIPALITY          = "ktm_owners"
-	URL_CONFISCATION_PER_MUNICIPALITY    = "ktm_confs"
-	URL_HORIZONTAL_AREA_PER_MUNICIPALITY = "ktm_harea"
-	URL_MORTGAGES_PER_MUNICIPALITY       = "ktm_liens"
+	URL_HORIZONTAL_PLOTS_PER_MUNICIPALITY      = "ktm_hplots"
+	URL_CONFISCATION_PER_MUNICIPALITY          = "ktm_confs"
+	URL_HORIZONTAL_AREA_PER_MUNICIPALITY       = "ktm_harea"
+	URL_MORTGAGES_PER_MUNICIPALITY             = "ktm_liens"
+	URL_TRANSACTIONS_PER_MUNICIPALITY          = "ktm_transactions"
+	URL_OWNERS_PER_MUNICIPALITY                = "ktm_owners"
+	URL_CADASTRE_STATUS_PER_MUNICIPALITY       = "ktm_status"
+	URL_CADASTRE_NATURA_PLOTS_PER_MUNICIPALITY = "cadastre_natura_plot"
+	URL_CADASTRE_PLOTS_PER_MUNICIPALITY        = "cadastre_plot"
+	URL_PLOTS_PER_MUNICIPALITY                 = "ktm_plots"
+	URL_RES_POWER_PRODUCTION                   = "admie_realtimescadares"
+	URL_POWER_SYSTEM_LOAD                      = "admie_realtimescadasystemload"
+	URL_ENERGY_BALANCE                         = "admie_dailyenergybalanceanalysis"
+	URL_ELECTRICITY_CONSUMPTION                = "electricity_consumption"
+	URL_CIVIL_PROTECTION_URBAN_INCIDENTS       = "mcp_urban_incidents"
+	URL_CIVIL_PROTECTION_FOREST_FIRES          = "mcp_forest_fires"
 
 	// Health.
 	// See more: https://data.gov.gr/search/?topic=health
-	URL_COVID_VACCINATION  = "mdg_emvolio"
-	URL_EFET_INSPECTIONS   = "efet_inspections"
-	URL_PHARMACIST_NUMBERS = "minhealth_pharmacists"
-	URL_PHARMACY_NUMBERS   = "minhealth_pharmacies"
-	URL_DENDIST_NUMBERS    = "minhealth_dentists"
-	URL_DOCTOR_NUMBERS     = "minhealth_doctors"
+	URL_COVID_VACCINATION = "mdg_emvolio"
+	URL_EFET_INSPECTIONS  = "efet_inspections"
+	URL_PHARMACISTS       = "minhealth_pharmacists"
+	URL_PHARMACIES        = "minhealth_pharmacies"
+	URL_DENDISTS          = "minhealth_dentists"
+	URL_DOCTORS           = "minhealth_doctors"
 
 	// Society.
 	// See more: https://data.gov.gr/search/?topic=society

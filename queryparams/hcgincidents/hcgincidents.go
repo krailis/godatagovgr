d3f4349729a28e150779694980aa381707c77759
Konstantinos Railis <krailis@hotmail.com>
2022-01-22 16:04:00 +0200

    Updated prefectures and HCG incidents query params

---
+++ queryparams/hcgincidents/hcgincidents.go
@@ -0,0 +1,26 @@
+package hcgincidents
+
+const (
+	BOAT_ABANDONMENT                    = "ΕΓΚΑΤΑΛΕΙΨΗ ΣΚΑΦΟΥΣ"
+	BOAT_WITHOUT_PASSENGERS_FINDING     = "ΑΝΕΥΡΕΣΗ ΣΚΑΦΟΥΣ ΧΩΡΙΣ ΕΠΙΒΑΙΝΟΝΤΕΣ"
+	CONFLICT                            = "ΣΥΓΚΡΟΥΣΗ"
+	COSPAS_SARSAT_TRANSMISSIONS         = "ΕΚΠΟΜΠΕΣ COSPAS/SARSAT"
+	EMERGENCY_LIGHTNING_SIGNS           = "ΦΩΤΙΣΤΙΚΑ ΣΗΜΑΤΑ ΚΙΝΔΥΝΟΥ"
+	FIRE                                = "ΠΥΡΚΑΪΑ"
+	GROUNDING                           = "ΠΡΟΣΑΡΑΞΗ"
+	ILLEGALLY_INCOMING_PERSONS          = "ΠΑΡΑΝΟΜΑ ΕΙΣΕΡΧΟΜΕΝΑ ΠΡΟΣΩΠΑ"
+	IMPACT                              = "ΠΡΟΣΚΡΟΥΣΗ"
+	MAN_IN_THE_SEA_FROM_SHIP            = "ΑΝΘΡΩΠΟΣ ΣΤΗ ΘΑΛΑΣΣΑ ΑΠΌ ΠΛΟΙΟ"
+	MECHANICAL_FAILURE                  = "ΜΗΧΑΝΙΚΗ ΒΛΑΒΗ"
+	MISSING_BOAT                        = "ΑΓΝΟΟΥΜΕΝΟ ΣΚΑΦΟΣ/ΛΕΜΒΟΣ"
+	MISSING_SWIMMER_DIVER_FISHERMAN     = "ΑΓΝΟΟΥΜΕΝΟΣ ΚΟΛΥΜΒΗΤΗΣ/ΔΥΤΗΣ/ΑΛΙΕΑΣ "
+	OTHER                               = "ΔΙΑΦΟΡΑ"
+	SHIP_IN_DIFFICULT_POSITION          = "ΠΛΟΙΟ/ΣΚΑΦΟΣ/ΛΕΜΒΟΣ ΣΕ ΔΥΣΧΕΡΗ ΘΕΣΗ"
+	SHIPPING_RISK                       = "ΝΑΥΤΙΛΙΑΚΟΣ ΚΙΝΔΥΝΟΣ (SECURITE)"
+	SHIPWRECK                           = "ΒΥΘΙΣΗ"
+	SHIPWRECK_DISCOVERY                 = "ΕΥΡΗΜΑΤΑ ΝΑΥΑΓΙΟΥ"
+	TRANSPORT_PATIENT_WOUNDED_FROM_SHIP = "ΜΕΤΑΦΟΡΑ ΑΣΘΕΝΗ/ΤΡΑΥΜΑΤΙΑ (ΑΠΌ ΠΛΟΙΟ)"
+	UNCONTROLLED_BOAT                   = "ΑΚΥΒΕΡΝΗΣΙΑ"
+	WATER_INFLOW                        = "ΕΙΣΡΟΗ ΥΔΑΤΩΝ"
+	WIRELESS_TRANSMISSIONS              = "ΑΣΥΡΜΑΤΕΣ ΕΚΠΟΜΠΕΣ"
+)
